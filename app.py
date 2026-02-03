3f5027e8b0e6a39fd940acecd09a6136a425d3b2
Nabil780554 <129799721+Nabil780554@users.noreply.github.com>
2026-02-03 20:23:35 +0100

    Initial commit - Test Streamlit

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Test SIRIUS - Déploiement Kubernetes")
+st.write("Si tu vois ce message, le conteneur fonctionne ! ✅")
+st.write(f"Application tournant sur le port 8501")
